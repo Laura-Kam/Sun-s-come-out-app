e370864e56a5c185a2e52530d2c6cd43ed4ca66a
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-08-06 13:31:06 +0100

    query selectors added

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,32 @@
-const btn = document.querySelector(#btn);
-const output = document.querySelector('.main-info-card')
-const url = 
+//variables created.
+
+const btn = document.querySelector(".btn");
+const output = document.querySelector(".main-info-card");
+let userInput = document.querySelector(".user-input");
+let userInputValue = document.querySelector(".user-input").value;
+
+// searching for city name using concat.
+
+const url =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=cd8545bb68e1aeb655a53433b147eb74";
+const APIkey = "cd8545bb68e1aeb655a53433b147eb74";
+var endPoint = "https://api.openweathermap.org/data/2.5/onecall?";
+
+var nameCity = "madrid";
+
+var searchCity =
+  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  nameCity +
+  "&appid=" +
+  APIkey;
+console.log(searchCity);
+
+// //on click fetch request works, Json data returned to console log.
+
+// btn.onclick = () => {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       addData(data);
+//     });
+// };
