b957b2e89b82e35928614b40109d304d461152bc
Laura Kambanis <laurakambanis@lauras-air-3.home>
2022-08-06 19:26:55 +0100

    attempt at local storage

--- assets/main.js
+++ assets/main.js
@@ -12,23 +12,124 @@ const url =
 const APIkey = "cd8545bb68e1aeb655a53433b147eb74";
 var endPoint = "https://api.openweathermap.org/data/2.5/onecall?";
 
-var nameCity = "madrid";
+//save to local storage.
+function searchMyCity() {
+  var cityName = document.getElementsByClassName(user - input).value;
+  var searchCity =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    nameCity +
+    "&appid=" +
+    APIkey;
+  fetch(searchCity)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  localStorage.setItem(searchMyCity);
+}
+//gets API temperature
 
-function searchCityWeatherAPI() {}
+function getAPITemperature() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=madrid&appid=cd8545bb68e1aeb655a53433b147eb74"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        console.log("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.main.temp);
+      document.querySelector(".temp").innerHTML =
+        "Temperature = " + data.main.temp;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+getAPITemperature();
 
-var searchCity =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  nameCity +
-  "&appid=" +
-  APIkey;
-console.log(searchCity);
+function getAPIWind() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=madrid&appid=cd8545bb68e1aeb655a53433b147eb74"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        console.log("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.wind.speed);
+      document.querySelector(".wind").innerHTML = "Wind = " + data.wind.speed;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getAPIWind();
+
+function getAPIUvi() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=madrid&appid=cd8545bb68e1aeb655a53433b147eb74"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        console.log("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.main);
+      document.querySelector(".uvi").innerHTML =
+        "UVI = " + data.main.wind_speed;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getAPIUvi();
+
+function getHumidity() {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=madrid&appid=cd8545bb68e1aeb655a53433b147eb74"
+  )
+    .then((response) => {
+      if (!response.ok) {
+        console.log("error");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.main);
+      document.querySelector(".humidity").innerHTML =
+        "Humidity = " + data.main.humidity;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+getHumidity();
+
+//get
+
+// var nameCity = "madrid";
+
+// var searchCity =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   nameCity +
+//   "&appid=" +
+//   APIkey;
+// console.log(searchCity);
 
 // //on click fetch request works, Json data returned to console log.
 
-btn.onclick = () => {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      addData(data);
-    });
-};
+// btn.onclick = () => {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       console.log(data);
+//     });
+// };
